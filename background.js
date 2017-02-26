f5de3f34003bf94211e07d75d6271ed49d909957
Kamil Florczyk <florczykkamil@gmail.com>
2017-02-26 17:42:58 +0100

    Searched word is pasted to dictionary website input and clicked search button instead of changing URL.

--- background.js
+++ background.js
@@ -1,48 +1,37 @@
 var SEARCH = "dictionary.cambridge.org";
+var SEARCH_FIELD_ID = "cdo-search-input";
+var SUBMIT_BUTTON = "button.cdo-search__button";
 var list = new InfinityFixedList(25);
 
-chrome.contextMenus.create({
-	title: "CambridgeDictionary: %s",
-	contexts: ["selection"],
-	onclick: function(info, tab) {
-		onContextMenuCommand("pl", info.selectionText.trim().toLowerCase());
-	}
-});
+(function initScript() {
+	chrome.contextMenus.create({ 
+		title: "CambridgeDictionary: %s",
+		contexts: ["selection"],
+		onclick: function(info, tab) { doTranslateSelection(info.selectionText.trim().toLowerCase(), "pl"); }
+	});
 
-chrome.commands.onCommand.addListener(function(command) {
-	if(command === "go-dictionary") {
-		onTranslateCommand("pl");
-	} else if(command === "go-dictionary-eng") {
-		onTranslateCommand("en");
-	}
-});
+	chrome.commands.onCommand.addListener(function(command) {
+		if(command === "go-dictionary")
+			onShortcutCommand("pl");
+		else if(command === "go-dictionary-eng")
+			onShortcutCommand("en");
+	});
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
-	
-	if(request.msg == "getList")
-	  sendResponse({list: list.getAll()});
-});
+	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+		//console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
+		if(request.msg == "getList")
+			sendResponse({list: list.getAll()});
+	});
+})();
 
-// redundant below two functions
-function onTranslateCommand(lang) {
-	searchDictionaryTab(function(tab) {
-		getSelection(function(word) {
-			if(tab == null) {
-				var href = "http://dictionary.cambridge.org/dictionary/" + (lang == "en" ? "english" : "english-polish") + "/" + word;
-				chrome.tabs.create({url: href}, function(tab) {
-					afterUpdateTab(tab);
-				});
-			} else {
-				updateTabUrl(word, lang, tab);
-			}
-
-			list.insert(word);
-		});
+// Main two in order first events
+function onShortcutCommand(lang) {
+	getSelection(function(word) {
+		doTranslateSelection(word, lang);
 	});
 }
 
-function onContextMenuCommand(lang, word) {
+function doTranslateSelection(word, lang) {
 	searchDictionaryTab(function(tab) {
 		if(tab == null) {
 			var href = "http://dictionary.cambridge.org/dictionary/" + (lang == "en" ? "english" : "english-polish") + "/" + word;
@@ -50,7 +39,8 @@ function onContextMenuCommand(lang, word) {
 				afterUpdateTab(tab);
 			});
 		} else {
-			updateTabUrl(word, lang, tab);
+			//updateTabUrl(word, lang, tab);
+			searchWordOnTargetPage(word, lang, tab);
 		}
 
 		list.insert(word);
@@ -101,6 +91,13 @@ function updateTabUrl(word, targetLang, dictionaryTab) {
 				afterUpdateTab(tab);
 		});
 	}
+}
+
+function searchWordOnTargetPage(word, targetLang, dictionaryTab) {
+	console.log(dictionaryTab);
+	chrome.tabs.executeScript(dictionaryTab.id, { code: 'document.getElementById("cdo-search-input").value = "'+word+'"; document.querySelector("button.cdo-search__button").click();' }, function(e) {
+		afterUpdateTab(dictionaryTab);
+	});
 }
 
 function getSelection(listener) {
